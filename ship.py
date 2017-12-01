8304d8348d8421340624febf77a12fef98dc33eb
yanbinbi <18244984914@163.com>
2017-12-01 15:21:05 +0800

    完成开发

---
+++ ship.py
@@ -0,0 +1,46 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Ship(Sprite):
+
+    def __init__(self, ai_settings, screen):
+        """初始化飞船并设置其初始位置"""
+        super(Ship, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # 加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/fighter.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # 将每艘新飞船放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # 在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
+
+        # 移动标志
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # self.rect.right 返回飞船外接矩形的右边缘的 x 坐标
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        # rect 的左边缘的 x 坐标大于零，就说明飞船未触及屏幕左边缘
+        elif self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # 根据self.center更新rect对象
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """让飞船在屏幕上居中"""
+        self.center = self.screen_rect.centerx
